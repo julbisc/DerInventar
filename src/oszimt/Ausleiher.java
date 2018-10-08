bb00e8379e2e7b294958888d970c6cb5ae0ac195
user <testset>
2018-10-08 16:18:14 +0200

    Attribute zu Klassen hinzugefügt.

--- src/oszimt/Ausleiher.java
+++ src/oszimt/Ausleiher.java
@@ -2,5 +2,26 @@ package oszimt;
 
 public class Ausleiher {
 
-    //Test
+    private static int ID = 0;
+    private String vorname;
+    private String nachname;
+    private String typ;
+
+    public Ausleiher(String vorname,String nachname,String typ){
+
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.typ = typ;
+
+        ID = ID + 1;
+
+    }
+
+    public void leihtAus(Leihobjekt leihobjekt){
+        DBManager.einpflegen(this, leihobjekt);
+    }
+
+    public void bekommtZurueck(Leihobjekt leihobjekt){
+        DBManager.loeschen(this, leihobjekt);
+    }
 }
--- src/oszimt/Leihobjekt.java
+++ src/oszimt/Leihobjekt.java
@@ -1,4 +1,14 @@
 package oszimt;
 
 public class Leihobjekt {
+    private static int ID = 0;
+    private String beschreibung;
+    private String typ;
+
+    public Leihobjekt(String beschreibung,String typ){
+        this.beschreibung = beschreibung;
+        this.typ = typ;
+
+        ID = ID + 1;
+    }
 }
